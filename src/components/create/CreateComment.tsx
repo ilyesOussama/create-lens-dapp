0d3c9dbeccbc62f22f3ad6d3f6d4cd32924ebffd
IlyasOussama <zghariilyasoussama@gmail.com>
2023-06-11 12:45:36 +0100

    create

---
+++ src/components/create/CreateComment.tsx
@@ -0,0 +1,10 @@
+import { useCreateComment } from "@lens-protocol/react-web";
+
+const CreateComment = () => {
+  /* 
+    const {excute,error,loading} = useCreateComment({}) */
+
+  return <div>CreateComment</div>;
+};
+
+export default CreateComment;
---
+++ src/components/create/CreatePost.tsx
@@ -0,0 +1,57 @@
+import {
+  ProfileOwnedByMe,
+  useCreatePost,
+  ContentFocus,
+} from "@lens-protocol/react-web";
+import { Button } from "../ui/button";
+import { useState } from "react";
+
+const CreatePost = ({ publisher }: { publisher: ProfileOwnedByMe }) => {
+  const [data, setData] = useState("");
+  const [transaction, setTransaction] = useState("");
+  const {
+    execute: create,
+    error,
+    isPending,
+  } = useCreatePost({ publisher, upload: uploadJson });
+
+  async function uploadJson(data: unknown) {
+    try {
+      const response = await fetch("/api/uploadToBundlr", {
+        method: "POST",
+        body: JSON.stringify(data),
+      });
+      const json = await response.json();
+      setTransaction(json.url);
+      return json.url;
+    } catch (err) {
+      console.log({ err });
+    }
+  }
+
+  async function createPost() {
+    await create({
+      content: data,
+      contentFocus: ContentFocus.TEXT_ONLY,
+      locale: "en",
+    });
+  }
+
+  return (
+    <div>
+      <input
+        placeholder="Create a post"
+        onChange={(e) => setData(e.target.value)}
+        className="text-black px-2 py-1"
+      />
+      <Button onClick={createPost}>Create Post</Button>
+      {transaction && (
+        <a target="_blank" rel="no-opener" href={transaction}>
+          View Arweave Data
+        </a>
+      )}
+    </div>
+  );
+};
+
+export default CreatePost;
