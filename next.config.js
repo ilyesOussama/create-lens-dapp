9a6e9e89325e5335a2a38598a8ada2d2c22728a4
IlyasOussama <zghariilyasoussama@gmail.com>
2023-06-11 12:43:27 +0100

    setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["lens.infura-ipfs.io"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
