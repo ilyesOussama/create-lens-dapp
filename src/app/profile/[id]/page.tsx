2dbae8c26d9dcbb73e48c9102cd83f34f677c5ba
IlyasOussama <zghariilyasoussama@gmail.com>
2023-06-13 19:29:58 +0100

    .

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,17 +1,14 @@
 "use client";
 
 import { Profile } from "@/components/profile";
-import { Publications } from "@/components/publication";
 import { ProfileId } from "@lens-protocol/react-web";
 import { useParams } from "next/navigation";
-import { LoginButton } from "@/components/auth";
 
 const ProfilePage = () => {
   const { id } = useParams();
   return (
     <div className="flex flex-col gap-4 border border-1 border-gray-200 dark:border-gray-700 container p-4 rounded-sm">
       <Profile profileId={id as ProfileId} />
-      <LoginButton />
     </div>
   );
 };
