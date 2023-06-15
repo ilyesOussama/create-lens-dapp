edd60c289e94e47c15b7f4953f2d6a549ade4354
IlyasOussama <zghariilyasoussama@gmail.com>
2023-06-16 00:54:14 +0100

    mirrored publications

--- src/components/publication/Publications.tsx
+++ src/components/publication/Publications.tsx
@@ -2,18 +2,27 @@ import {
   AnyPublication,
   ProfileId,
   usePublications,
+  PaginatedReadResult,
 } from "@lens-protocol/react-web";
 import { Publication } from "./Publication";
 import { useEffect, useState } from "react";
 import { cn } from "@/lib/utils";
+import { Repeat } from "lucide-react";
 
-const Publications = ({ profileId }: { profileId: ProfileId }) => {
+const Publications = ({
+  profileId,
+  limit = 10,
+}: {
+  profileId: ProfileId;
+  limit?: number;
+}) => {
   const [publications, setPublications] = useState<
     AnyPublication[] | undefined
   >([]);
 
   const { data, error, loading } = usePublications({
     profileId,
+    limit,
   });
 
   useEffect(() => {
@@ -35,18 +44,37 @@ const Publications = ({ profileId }: { profileId: ProfileId }) => {
 
   return (
     <div className="mx-auto flex flex-col gap-4 border border-1 border-gray-200 dark:border-gray-700 rounded-sm">
-      {publications?.map(({ id }, index) => (
-        <div
-          key={id}
-          className={cn(
-            index !== data.length - 1 &&
-              "border-b border-gray-200 dark:border-gray-700",
-            "py-2"
-          )}
-        >
-          <Publication publicationId={id} />
-        </div>
-      ))}
+      {publications?.map((publication, index) => {
+        if (publication.__typename === "Mirror") {
+          return (
+            <div
+              key={publication.id}
+              className={cn(
+                index !== data.length - 1 &&
+                  "border-b border-gray-200 dark:border-gray-700",
+                "py-2"
+              )}
+            >
+              <div className="flex flex-row gap-2 px-4">
+                <Repeat /> {publication.profile.name} mirrored
+              </div>
+              <Publication publicationId={publication.mirrorOf.id} />
+            </div>
+          );
+        }
+        return (
+          <div
+            key={publication.id}
+            className={cn(
+              index !== data.length - 1 &&
+                "border-b border-gray-200 dark:border-gray-700",
+              "py-2"
+            )}
+          >
+            <Publication publicationId={publication.id} />
+          </div>
+        );
+      })}
     </div>
   );
 };
